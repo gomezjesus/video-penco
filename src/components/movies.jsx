e8a27561177996b6fcdfb4c52a68b840bc555f4a
Jesus Gerardo Gomez Betancourt <jgb@Jesuss-MacBook-Air.local>
2020-03-12 19:09:06 -0600

    Second commit

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -5,32 +5,52 @@ class Movies extends Component {
   state = {
     movies: getMovies()
   };
+  handleDelete = movie => {
+    console.log(movie);
+    const movies = this.state.movies.filter(m => m._id !== movie._id);
+    this.setState({ movies: movies });
+  };
+
   constructor() {
     super();
   }
 
   render() {
+    if (this.state.movies.length === 0)
+      return <p>There are no movies on the database</p>;
     return (
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">Title</th>
-            <th scope="col">Genre</th>
-            <th scope="col">Stock</th>
-            <th scope="col">Rate</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.movies.map(movie => (
-            <tr key={movie._id}>
-              <td>{movie.title}</td>
-              <td>{movie.genre.name}</td>
-              <td>{movie.numberInStock}</td>
-              <td>{movie.dailyRentalRate}</td>
+      <div>
+        <p>Showing {this.state.movies.length} movies in the database</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Rate</th>
+              <th />
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {this.state.movies.map(movie => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
